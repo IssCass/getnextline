78b6b805e24c212e1f999f9049cdb1f5e9219e38
Enzo Ventura <enzo.ventura@epitech.eu>
2020-08-24 11:43:25 +0200

    [+] Added finished project

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** .
+** File description:
+** Created by Enzo Ventura
+*/
+
+#ifndef READ_SIZE
+#define READ_SIZE   100
+
+char *get_next_line(int fd);
+char one_char(int fd);
+
+#endif //_GET_NEXT_LINE_H
